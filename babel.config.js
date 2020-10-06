f41930983747d1b11c06dced79e81cb39996d07b
Gerald <wzcnhgf@gmail.com>
2020-10-06 19:26:38 +0800

    add 绝对路径2

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/utils': './src/utils',
+          '@/config': './src/config',
+          '@/navigator': './src/navigator',
+          '@/models': './src/models',
+          '@/pages': './src/pages',
+          '@/assets': './src/assets',
+          '@/components': './src/components',
+        },
+      },
+    ],
+  ],
 };
