80158897a5fc8129fabc0d5b6c40ee8259155adb
Gerald <wzcnhgf@gmail.com>
2020-10-07 19:22:23 +0800

    8-3

---
+++ src/models/home.ts
@@ -0,0 +1,44 @@
+import {Effect, Model} from 'dva-core-ts';
+import {Reducer} from 'redux';
+
+interface HomeState {
+  num: number;
+}
+
+const action = {
+  type: 'add',
+};
+
+interface HomeModel extends Model {
+  namespace: 'home';
+  state?: {
+    num: number;
+  };
+  reducers: {
+    add: Reducer<HomeState>;
+  };
+  effects?: {
+    asyncAdd: Effect;
+  };
+}
+
+const initialState = {
+  num: 0,
+};
+
+const homeModel: HomeModel = {
+  namespace: 'home',
+  state: {
+    num: 0,
+  },
+  reducers: {
+    add(state = initialState, {payload}) {
+      return {
+        ...state,
+        num: state?.num + payload.num,
+      };
+    },
+  },
+};
+
+export default homeModel;
