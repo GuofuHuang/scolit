9d6b3828a6c10b58efada85635e27e1757c1baeb
Gerald <wzcnhgf@gmail.com>
2020-10-11 12:37:37 +0800

    10-3

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,11 @@
 import home from './home';
 import {DvaLoadingState} from 'dva-loading-ts';
-
-const models = [home];
+import category from '@/models/category';
+const models = [home, category];
 
 export type RootState = {
   home: typeof home.state;
+  category: typeof category.state;
   loading: DvaLoadingState;
 };
 
